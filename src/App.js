3108439ce6022f2cf275c56c9ea44fc43732db35
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-12-06 16:30:59 -0500

    initial commit with boilerplate elements

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import MiniDrawer from './components/MiniDrawer'
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        <MiniDrawer />
+      </div>
+    );
+  }
+}
+
+export default App;
