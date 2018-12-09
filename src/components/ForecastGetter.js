688e751273b6d34c711b4402ed72130dabf40c95
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-12-08 19:17:46 -0500

    add location getter WIP

--- src/components/ForecastGetter.js
+++ src/components/ForecastGetter.js
@@ -1,78 +1,113 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import classnames from 'classnames';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import red from '@material-ui/core/colors/red';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import Input from '@material-ui/core/Input';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import classnames from "classnames";
 
-import MiniParticlesContainer from './MiniParticlesContainer'
+import Button from "@material-ui/core/Button";
+
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import red from "@material-ui/core/colors/red";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import Input from "@material-ui/core/Input";
+
+import MiniParticlesContainer from "./MiniParticlesContainer";
+
+import Geocode from "react-geocode";
 
 const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit
+  },
   card: {
     maxWidth: 400,
-    margin: '50px auto'
+    margin: "50px auto"
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%" // 16:9
   },
   actions: {
-    display: 'flex',
+    display: "flex"
   },
   expand: {
-    transform: 'rotate(0deg)',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
+    transform: "rotate(0deg)",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest
     }),
-    marginLeft: 'auto',
-    [theme.breakpoints.up('sm')]: {
-      marginRight: -8,
-    },
+    marginLeft: "auto",
+    [theme.breakpoints.up("sm")]: {
+      marginRight: -8
+    }
   },
   expandOpen: {
-    transform: 'rotate(180deg)',
+    transform: "rotate(180deg)"
   },
   avatar: {
-    backgroundColor: red[500],
+    backgroundColor: red[500]
   },
   input: {
     margin: theme.spacing.unit,
-  },
+    width: 200
+  }
 });
 
-
-
-
 class ForecastGetter extends React.Component {
-  state = {
-    expanded: false,
-    locationString: 'cheese'
-  };
+  constructor(props) {
+    super(props)
 
-  componentDidMount(){
-    if (navigator.geolocation) {
-      this.setState({locationString: navigator.geolocation.getCurrentPosition()})
-    } else {
-      this.setState({locationString: 'cannot get location'})
-    }
+    this.state = {
+      expanded: false,
+      lat: 0,
+      lng: 0,
+      locationString: '',
+      coords: {},
+      value: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleExpandClick = () => {
     this.setState(state => ({ expanded: !state.expanded }));
   };
+
+  handleChange(event) {
+    this.setState({locationString: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    if (this.state.locationString === `${this.state.lat},${this.state.lng}`) {
+      console.log('retrieved location submitted')
+    } else {
+      console.log('A name was submitted: ' + this.state.locationString);
+    }
+  }
+
+  componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.setState(state => ({
+          coords: position.coords,
+          lat: position.coords.latitude.toFixed(4),
+          lng: position.coords.longitude.toFixed(4),
+          locationString: `${position.coords.latitude.toFixed(4)},${position.coords.longitude.toFixed(4)}`
+        }))
+      })
+    }
+  }
 
   render() {
     const { classes } = this.props;
@@ -96,18 +131,30 @@ class ForecastGetter extends React.Component {
 
         <MiniParticlesContainer />
 
-
         <CardContent>
-          <Typography component="p">
-            Let's start with a location:
-          </Typography>
-          <Input
-        defaultValue={this.state.locationString}
-        className={classes.input}
-        inputProps={{
-          'aria-label': 'Description',
-        }}
-      />
+          <Typography component="p">Let's start with a location:</Typography>
+          <form onSubmit={this.handleSubmit}>
+            <Input
+              placeholder='waiting for location...'
+              className={classes.input}
+              inputProps={{
+                "aria-label": "Description"
+              }}
+              value={this.state.locationString}
+              onChange={this.handleChange}
+            />
+
+
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              type="submit"
+              value="Submit"
+            >
+              drizzl it!
+            </Button>
+          </form>
         </CardContent>
         <CardActions className={classes.actions} disableActionSpacing>
           <IconButton aria-label="Add to favorites">
@@ -119,7 +166,7 @@ class ForecastGetter extends React.Component {
 
           <IconButton
             className={classnames(classes.expand, {
-              [classes.expandOpen]: this.state.expanded,
+              [classes.expandOpen]: this.state.expanded
             })}
             onClick={this.handleExpandClick}
             aria-expanded={this.state.expanded}
@@ -132,9 +179,7 @@ class ForecastGetter extends React.Component {
         <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
           <CardContent>
             <Typography paragraph>about:</Typography>
-            <Typography paragraph>
-              Made with material ui
-            </Typography>
+            <Typography paragraph>Made with material ui</Typography>
           </CardContent>
         </Collapse>
       </Card>
@@ -143,7 +188,7 @@ class ForecastGetter extends React.Component {
 }
 
 ForecastGetter.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(ForecastGetter);
